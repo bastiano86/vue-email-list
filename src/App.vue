c9dfdcd7dba96280172a71bee78f575947a3dae1
Sebastiano La Monaca <sebastiano_lamonaca@yahoo.com>
2021-09-28 17:04:08 +0200

    Esercizio completato

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div v-for="mailRandomica in 10" :key="10">
-      {{ mailRandomica }}
-    </div>
+    <HelloWorld msg="Welcome to our first mail list getter"/>
+    <div v-for="mail in listaMail" :key="mail">{{ mail }}</div>
   </div>
 </template>
 
@@ -19,17 +17,20 @@ export default {
   },
   data: function (){
     return{
-     mailRandomica : 0
+    listaMail:[],
     }
   },
   mounted(){
+    for ( let i = 0; i < 10; i ++ ) {
     console.log(axios);
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then((response)=> {
       const result = response.data;
       console.log(result.response);
-      this.mailRandomica = result.data.response;
+      this.listaMail.push(result.response);
     })
+    }
+    console.log(this.listaMail)
   }
 }
 </script>
