0ae21af3013b6cebacbfaf790fa66e0f16910889
Sebastiano La Monaca <sebastiano_lamonaca@yahoo.com>
2021-09-28 15:54:31 +0200

    API di mail random implementata

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,34 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="mailRandomica in 10" :key="10">
+      {{ mailRandomica }}
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios"
 export default {
+
   name: 'App',
   components: {
     HelloWorld
+  },
+  data: function (){
+    return{
+     mailRandomica : 0
+    }
+  },
+  mounted(){
+    console.log(axios);
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((response)=> {
+      const result = response.data;
+      console.log(result.response);
+      this.mailRandomica = result.data.response;
+    })
   }
 }
 </script>
